4c3b814ec6938f4d0ca3edc82dca831ccb578227
beardad1975 <beardad1975@nmes.tyc.edu.tw>
2021-10-26 20:18:30 +0800

    bumped version 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="turtle4t",
-    version="0.0.2",
+    version="0.0.3",
     author="Wen-Hung, Chang 張文宏",
     author_email="beardad1975@nmes.tyc.edu.tw",
     description="turtle for Teenagers",
